b759c5096e951ac08b7b55b8ed9ce0e9e8f8446b
Melanie Sumner <melaniersumner@gmail.com>
2020-01-28 19:02:35 -0600

    adding a native event

--- app/components/native-events.js
+++ app/components/native-events.js
@@ -1,4 +1,9 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
 export default class NativeEventsComponent extends Component {
+  @action
+  updateName(val) {
+    console.log('update', val)
+  }
 }
